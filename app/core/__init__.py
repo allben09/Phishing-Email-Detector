0544f090189f1d11e28a95ca78bee3b85e966ee1
Allben Rakgoale <refallben@gmail.com>
2026-07-22 23:21:19 +0200

    Initialize core module with imports for detectors

---
+++ app/core/__init__.py
@@ -0,0 +1,6 @@
+from .detector import AdvancedPhishingDetector, AnalysisResult
+from .ml_detector import MLPhishingDetector
+from .header_analyzer import EmailHeaderAnalyzer
+from .url_analyzer import AdvancedURLAnalyzer
+from .attachment_analyzer import AttachmentAnalyzer
+from .threat_intel import ThreatIntelligence
